2dea6d7de8096aaacc8ebabe989155aced1e1625
zaid <zaid2023.ae@gmail.com>
2023-12-25 19:20:50 +0400

    completed

--- browse.js
+++ browse.js
@@ -2,3 +2,31 @@ function toggleNav() {
     var nav = document.querySelector('nav');
     nav.classList.toggle('show');
 }
+
+
+
+  // Sample car inventory data (replace this with data from your API)
+  const carInventoryData = [
+    { make: 'Toyota', model: 'Camry', year: 2022, price: 25000 },
+    { make: 'Honda', model: 'Accord', year: 2021, price: 23000 },
+    { make: 'Ford', model: 'Mustang', year: 2023, price: 35000 },
+    // Add more cars as needed
+];
+
+// Function to display the car inventory on the page
+function displayCarInventory() {
+    const carInventoryContainer = document.getElementById('carInventory');
+    carInventoryContainer.innerHTML = ''; // Clear previous content
+
+    carInventoryData.forEach(car => {
+        const carElement = document.createElement('div');
+        carElement.innerHTML = `<h2>${car.make} ${car.model}</h2>
+                               <p>Year: ${car.year}</p>
+                               <p>Price: $${car.price}</p>`;
+        carInventoryContainer.appendChild(carElement);
+    });
+}
+
+// Load and display the car inventory when the page loads
+window.onload = displayCarInventory;
+
